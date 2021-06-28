8af50a196de0c83f44977b14aaa370c9aa20c39b
RekiDunois <rekidunois+github@gmail.com>
2021-06-28 15:56:32 +0800

    test✅:  add Square test

---
+++ tests/Square.test.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import Square, { Props } from "../src/component/Square";
+import { async } from "q";
+
+describe("Single Square testing", () => {
+    test("Square", async () => {
+        const clickFun = jest.fn();
+        const prop: Props = {
+            onClick: clickFun,
+            value: ''
+        }
+        const { getByRole } = render(<Square value={prop.value} onClick={() => prop.onClick(null)} />)
+        const btn = getByRole("button");
+        fireEvent.click(btn ? btn : null);
+        expect(clickFun).toBeCalled()
+    });
+});
